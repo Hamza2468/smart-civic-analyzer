c6907a2b3365b0e028d9ce16a45fd80009256fdf
Hamza2468 <105994979+Hamza2468@users.noreply.github.com>
2026-04-28 18:10:19 +0500

    updated

--- backend/main.py
+++ backend/main.py
@@ -27,6 +27,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+       
         "https://smart-civic-analyzer-pe1s.vercel.app"
     ],
     allow_credentials=True,
@@ -186,4 +187,4 @@ def get_summary(db: Session = Depends(get_db)):
 
 # ─── Run ──────────────────────────────────────────────────────────────────────
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
